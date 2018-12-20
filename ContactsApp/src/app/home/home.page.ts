77e918adbf83361241caab970ee07d5e282e9929
Anshul Daga <anshuldaga@gmail.com>
2018-12-20 14:08:26 -0500

    Angular Basics Videos 1-9

--- ContactsApp/src/app/home/home.page.ts
+++ ContactsApp/src/app/home/home.page.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  dateTimeMmessage : string;
+
+  constructor()
+  {
+     console.log("I am in constructor method ");
+     setInterval(()=>this.myMethod(),1000);
+
+
+     //this.myMethod();
+  }
+
+
+  myMethod(){
+
+  let currentDtTime = new Date();
+     this.dateTimeMmessage = currentDtTime.toDateString()+ "   "+ currentDtTime.toTimeString();
+
+    console.log("I am in my method... ");
+  }
+  
+
 }
