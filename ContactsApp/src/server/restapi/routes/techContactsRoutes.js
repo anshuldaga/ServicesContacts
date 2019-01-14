c54b9de49edade97d9b20d06bb626f1138316fbb
ashish daga <44991330+touch2info@users.noreply.github.com>
2019-01-14 00:55:40 -0500

    comitting again as somethign went wrong

--- ContactsApp/src/server/restapi/routes/techContactsRoutes.js
+++ ContactsApp/src/server/restapi/routes/techContactsRoutes.js
@@ -4,6 +4,25 @@ module.exports = function(app) {
     console.log("@@ inside Router export method");
     var techContacts = require('../controllers/techContactsController');
 
+
+//This is to bypass CORS and allow browser to access diff domain. Change it as per u deploy code on prod server.
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+  console.log("XXX in CORS get method ");
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+ console.log("YYY in CORS post method ");
+});
+
+
+
     app.route('/getTechList')
         .get(techContacts.getTechList);
     app.route('/getTechDetails/:techID')
