648b07956bf4e6173f8316af96c8ab8f24502b5a
ashish daga <44991330+touch2info@users.noreply.github.com>
2019-01-16 00:08:55 -0500

    added new page for tech search as per skill sets

---
+++ ContactsApp/src/app/tech-search/tech-search.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TechSearchPage } from './tech-search.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TechSearchPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TechSearchPage]
+})
+export class TechSearchPageModule {}
---
+++ ContactsApp/src/app/tech-search/tech-search.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tech-search',
+  templateUrl: './tech-search.page.html',
+  styleUrls: ['./tech-search.page.scss'],
+})
+export class TechSearchPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
