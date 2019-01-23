9b96878dae76928be931f84b234fa8045f0beaad
ashish daga <44991330+touch2info@users.noreply.github.com>
2019-01-22 22:46:45 -0500

    enabled PWA by adding require files via angular
    enabled PWA by adding require files via angular.

--- ContactsApp/src/app/app.module.ts
+++ ContactsApp/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
     StatusBar,
     SplashScreen,
